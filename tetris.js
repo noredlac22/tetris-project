050684b5e0099eda9cca200ac43d5f8c930cf22f
noredlac22 <christianqtc@gmail.com>
2020-03-01 23:33:56 -0800

    draw and undraw functions to move piece

--- tetris.js
+++ tetris.js
@@ -9,10 +9,10 @@ const VACANT = "WHITE"; // color of empty square
 // draw a square in the canvas with unit SQ
 function drawSquare(x,y,color){
     ctx.fillStyle = color;
-    ctx.fillRect(x*SQ,y*SQ,50,50);
+    ctx.fillRect(x*SQ,y*SQ,SQ,SQ);
 
     ctx.strokeStyle = "BLACK";
-    ctx.strokeRect(x*SQ,y*SQ,50,50);
+    ctx.strokeRect(x*SQ,y*SQ,SQ,SQ);
 }
 
 
@@ -21,7 +21,97 @@ function drawSquare(x,y,color){
 let board = [];
 for(r = 0; r < ROW; r++){
     board[r] = [];
-    for(c = 0; c < COLUMN; c++){
+    for(c = 0; c < COL; c++){
         board[r][c] = VACANT;
     }
 }
+
+// draw the board to canvas
+function drawBoard(){
+    for(r = 0; r < ROW; r++){
+        for(c = 0; c < COL; c++){
+            drawSquare(c,r,board[r][c]);
+        }
+    }
+}
+
+drawBoard();
+
+// the pieces and the colors
+
+const PIECES = [
+    [Z,"red"],
+    [S,"green"],
+    [T,"purple"],
+    [O,"yellow"],
+    [L,"blue"],
+    [I,"cyan"],
+    [J,"orange"],
+];
+
+// initiate a piece
+
+let p = new Piece(PIECES[0][0],PIECES[0][1]);
+
+// draw the Object Piece
+
+function Piece(tetrimino,color){
+    this.tetrimino = tetrimino;
+    this.color = color;
+
+    this.tetriminoN = 0; // start with the first pattern(layout)
+    this.activeTetrimino = this.tetrimino[this.tetriminoN];
+
+    // to move or control the pieces
+    this.x = 3;
+    this.y = 0;
+}
+
+// draw a piece to the board
+
+Piece.prototype.draw = function(){
+    for(r = 0; r < this.activeTetrimino.length; r++){
+        for(c = 0; c < this.activeTetrimino.length; c++){
+            // we draw only the occupied squares
+            if(this.activeTetrimino[r][c]){
+                drawSquare(this.x + c, this.y + r, this.color);
+            }
+        }
+    }
+}
+
+// undraw a piece (for movement)
+
+Piece.prototype.unDraw = function(){
+    for(r = 0; r < this.activeTetrimino.length; r++){
+        for(c = 0; c < this.activeTetrimino.length; c++){
+            // we draw only the occupied squares
+            if(this.activeTetrimino[r][c]){
+                drawSquare(this.x + c, this.y + r, VACANT);
+            }
+        }
+    }
+}
+
+// move down the piece each time
+
+Piece.prototype.moveDown = function(){
+    this.unDraw();
+    this.y++;
+    this.draw();
+}
+
+// drop the piece every second
+
+let dropStart = Date.now();
+function drop(){
+    let now = Date.now();
+    let delta = now - dropStart;
+    if(delta > 1000){
+        p.moveDown();
+        dropStart = Date.now();
+    }
+    requestAnimationFrame(drop);
+}
+
+drop();
